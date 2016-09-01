6464bb4da0f24ca3c284138e5fe1be65354dbf74
root <root@cms-phedex-plots.cern.ch>
2016-09-01 15:37:19 +0200

    A little of documentation

---
+++ cron/collect.sh
@@ -0,0 +1,8 @@
+now=$(date +'%Y-%m-%d')
+filename=`date +%Y-%m-%d -d "yesterday"`
+sourcedir=/cms/phedex-monitoring
+outputdir=out
+headers=now,br_user_group,data_tier,acquisition_era,node_kind,br_dest_bytes,br_node_bytes
+
+hadoop fs -getmerge $sourcedir/$now* $outputdir/$filename
+sed -i '1s/^/'$headers'\n/' $outputdir/$filename
---
+++ cron/remove.sh
@@ -0,0 +1,4 @@
+filename=`date +%Y-%m-%d -d "3 months ago"`
+outputdir=out
+
+rm $outputdir/$filename
